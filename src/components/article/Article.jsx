8fb9c7afe683eb83144db2e404f5380802738fdc
amisiespoir-07 <espoirlumbu@gmail.com>
2023-01-26 15:56:06 +0800

    Blog Page Error

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './article.css';
+
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <div className='gpt3__article'>
+
+      <div className='gpt3__aticle-image'>
+        <img src={imgUrl} alt="blog" />
+      </div>
+
+      <div className='gpt3__article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+}
+
+export default Article
